58ef2d06863b3e587159e20d5367612e7b70e80e
unknown <atul_0012@yahoo.co.in>
2022-07-22 14:09:28 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import UserDetails from "./UsersDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            users: [],
+            DataisLoaded: false,
+        };
+        
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        fetch(
+        "https://602e7c2c4410730017c50b9d.mockapi.io/users")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    users: json,
+                    DataisLoaded: true
+                });
+            })
+    }        
+    
+    clickMe(userDetails){
+      console.log(userDetails);
+       
+    }
+    render() {
+        const { DataisLoaded, users } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Please wait some time.... </h1> </div> ;
+   
+        return (
+        <div className = "App">
+            <h1> USERS LIST </h1>  {
+                users.map((user, index) => ( 
+                <ol key = { user.id } >
+                    <h3 onClick={this.clickMe.bind(this, user)} > 
+                      {user.profile.firstName}   {user.profile.lastName}
+                    </h3> 
+                    <p>
+                      Job Title: {user.jobTitle}
+                    </p>
+                    <p>
+                      Email: {user.profile.email}
+                    </p>
+                    
+                    </ol>
+                ))
+            }
+        <UserDetails  />
+            
+        </div>
+        
+        
+    );
+
+    
+}
 }
 
+
+   
 export default App;
+
+
+
+
---
+++ src/UsersDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+const UserDetails = () => {
+
+    
+
+    return(
+        <h1>USER DETAILS</h1>
+    )
+   
+}
+
+
+export default UserDetails;
